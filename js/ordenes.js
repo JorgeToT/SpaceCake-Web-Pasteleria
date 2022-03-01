d3c8f5dbde0859df3f8205a1226b38e23238a409
JorgeToT <jorge800.azul@gmail.com>
2022-03-01 03:17:55 -0600

    Avance

---
+++ js/ordenes.js
@@ -0,0 +1,9 @@
+function recogerDatos(){
+    let nombre = document.getElementById("nombreCliente").value;
+    let telefono = document.getElementById("telefonoCliente").value;
+    let correo = document.getElementById("correoCliente").value;
+
+    let mensaje = nombre+" "+telefono+""+correo
+
+    console.log(mensaje);
+}
